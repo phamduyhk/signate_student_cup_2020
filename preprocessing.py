35ca87a7d469519ddcb0df58a5e689f4c3fc9886
phamduyhk <phamduyhk@gmail.com>
2020-08-06 12:45:54 +0900

    update preprocessing with k fold

--- preprocessing.py
+++ preprocessing.py
@@ -3,6 +3,136 @@
 Author: Pham Duy
 Created date: 2020/08/05
 """
+import re
+import logging
+
+import numpy as np
+import pandas as pd
+import spacy
+import torch
+from joblib import Memory
+from torchtext import data
+from sklearn.model_selection import KFold
+
+NLP = spacy.load('en')
+MAX_CHARS = 20000
+LOGGER = logging.getLogger("SIGNATE STUDENT CUP DATASET")
+MEMORY = Memory(cachedir="cache/", verbose=1)
+
+
+def tokenizer(description):
+    description = re.sub(
+        r"[\*\"“”\n\\…\+\-\/\=\(\)‘•:\[\]\|’\!;]", " ", str(description))
+    description = re.sub(r"[ ]+", " ", description)
+    description = re.sub(r"\!+", "!", description)
+    description = re.sub(r"\,+", ",", description)
+    description = re.sub(r"\?+", "?", description)
+    if (len(description) > MAX_CHARS):
+        description = description[:MAX_CHARS]
+    return [x.text for x in NLP.tokenizer(description) if x.text != " "]
+
+
+def prepare_csv():
+    df_train = pd.read_csv("data/train.csv")
+    df_train["description"] = df_train.description.str.replace("\n", " ")
+    df_train.to_csv("cache/dataset_train.csv", index=False)
+    df_test = pd.read_csv("data/test.csv")
+    df_test["description"] = df_test.description.str.replace("\n", " ")
+    df_test.to_csv("cache/dataset_test.csv", index=False)
+
+
+@MEMORY.cache
+def read_files(fix_length=100, lower=False, vectors=None):
+    if vectors is not None:
+        # pretrain vectors only support all lower case
+        lower = True
+    LOGGER.debug("Preparing CSV files...")
+    prepare_csv()
+    description = data.Field(
+        sequential=True,
+        fix_length=fix_length,
+        tokenize=tokenizer,
+        pad_first=True,
+        lower=lower
+    )
+    LOGGER.debug("Reading train csv file...")
+    train = data.TabularDataset(
+        path='cache/dataset_train.csv', format='csv', skip_header=True,
+        fields=[
+            ('id', None),
+            ('description', description),
+            ('jobflag', data.Field(
+                use_vocab=False, sequential=False))
+        ])
+    LOGGER.debug("Reading test csv file...")
+    test = data.TabularDataset(
+        path='cache/dataset_test.csv', format='csv', skip_header=True,
+        fields=[
+            ('id', None),
+            ('description', description)
+        ])
+    LOGGER.debug("Building vocabulary...")
+    description.build_vocab(
+        train, test,
+        max_size=20000,
+        min_freq=50,
+        vectors=vectors
+    )
+    LOGGER.debug("Done preparing the datasets")
+    return train.examples, test.examples, description
+
+"""
+K FOLD USAGE
+
+# # k分割交差検証（k-fold cross-validation）
+# kf = KFold(n_splits=5, shuffle=True)
+# print(train_val_ds)
+# for train_index, test_index in kf.split(train_val_ds):
+#     print(train_index, test_index)
+#
+# # 一つ抜き交差検証（leave-one-out cross-validation）
+# loo = LeaveOneOut()
+# for train, test in loo.split(train_val_ds):
+#     print("train {}, test {}".format(train, test))
+#
+# # 層化k分割交差検証（stratified k-fold cross-validation）
+# skf = StratifiedKFold(n_splits=3)
+# for train, test in skf.split(train_val_ds):
+#     print("train {}, test {}".format(train, test))
+"""
+
+def get_dataset(fix_length=100, lower=False, vectors=None, n_folds=5, seed=999):
+    train_exs, test_exs, description = read_files(
+        fix_length=fix_length, lower=lower, vectors=vectors)
+
+    kf = KFold(n_splits=n_folds, random_state=seed)
+
+    fields = [
+        ('id', None),
+        ('description', description),
+        ('jobflag', data.Field(
+            use_vocab=False, sequential=False))
+    ]
+
+    def iter_folds():
+        train_exs_arr = np.array(train_exs)
+        for train_idx, val_idx in kf.split(train_exs_arr):
+            yield (
+                data.Dataset(train_exs_arr[train_idx], fields),
+                data.Dataset(train_exs_arr[val_idx], fields),
+            )
+
+    test = data.Dataset(test_exs, fields[:2])
+    return iter_folds(), test
+
+
+def get_iterator(dataset, batch_size, device=0, train=True, shuffle=True, repeat=False):
+    dataset_iter = data.Iterator(
+        dataset, batch_size=batch_size, device=device,
+        train=train, shuffle=shuffle, repeat=repeat,
+        sort=False
+    )
+    return dataset_iter
 
 
 def preprocessing(file_path):
@@ -15,3 +145,20 @@ def preprocessing(file_path):
         torch formed dataloader
     """
     return None
+
+
+if __name__ == '__main__':
+    train_val_generator, test_dataset = get_dataset(
+        fix_length=100, lower=True, vectors="fasttext.en.300d",
+        n_folds=5, seed=123
+    )
+    for fold, (train_dataset, val_dataset) in enumerate(train_val_generator):
+        for batch in get_iterator(
+                train_dataset, batch_size=32, train=True,
+                shuffle=True, repeat=False
+        ):
+            x = batch.description.data
+            y = batch.jobflag
+            print(x)
+            print(y)
+            break
--- train.py
+++ train.py
@@ -9,6 +9,7 @@ import torch
 
 # preprocessing module
 from utils.dataloader import Preprocessing
+from preprocessing import get_iterator, get_dataset
 
 # Preliminaries
 
@@ -27,6 +28,7 @@ import torch.optim as optim
 
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix, f1_score
 import seaborn as sns
+from sklearn.model_selection import KFold, LeaveOneOut, StratifiedKFold
 
 preprocessor = Preprocessing()
 
@@ -45,7 +47,7 @@ text_field = Field(use_vocab=False, tokenize=tokenizer.encode, lower=False, incl
 fields = [('text', text_field), ('label', label_field)]
 
 # SETTINGs
-device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 destination_folder = "./output"
 if not os.path.exists(destination_folder):
     os.mkdir(destination_folder)
@@ -54,20 +56,42 @@ source_folder = "./data"
 # Get data (ver transformer)
 vector_list_file = "wiki-news-300d-1M.vec"
 max_sequence_length = 256
-batch_size = 16
+batch_size = 32
 
-# TabularDataset Ver
-train_val_ds, test = TabularDataset.splits(path=source_folder, train='train_data.csv',
-                                           test='test_data.csv', format='CSV', fields=fields, skip_header=True)
 
-train, valid = train_val_ds.split(
-    split_ratio=0.7, random_state=random.seed(2395))
-
-train_iter = BucketIterator(train, batch_size=batch_size, sort_key=lambda x: len(x.text),
-                            device=device, train=True, sort=True, sort_within_batch=True, shuffle=False)
-valid_iter = BucketIterator(valid, batch_size=batch_size, sort_key=lambda x: len(x.text),
-                            device=device, train=True, sort=True, sort_within_batch=True, shuffle=False)
-test_iter = Iterator(test, batch_size=batch_size, device=device, train=False, shuffle=False, sort=False)
+# # TabularDataset Ver
+# train_val_ds, test = TabularDataset.splits(path=source_folder, train='train_data.csv',
+#                                            test='test_data.csv', format='CSV', fields=fields, skip_header=True)
+#
+# train, valid = train_val_ds.split(
+#     split_ratio=0.7, random_state=random.seed(2395))
+#
+# # train_iter = BucketIterator(train, batch_size=batch_size, sort_key=lambda x: len(x.text),
+# #                             device=device, train=True, sort=True, sort_within_batch=True, shuffle=False)
+# # valid_iter = BucketIterator(valid, batch_size=batch_size, sort_key=lambda x: len(x.text),
+# #                             device=device, train=True, sort=True, sort_within_batch=True, shuffle=False)
+#
+#
+# train_iter = BucketIterator(train, batch_size=batch_size, device=device, train=True, shuffle=False)
+# valid_iter = BucketIterator(valid, batch_size=batch_size,
+#                             device=device, train=True, shuffle=False)
+# test_iter = Iterator(test, batch_size=batch_size, device=device, train=False, shuffle=False, sort=False)
+#
+# # k分割交差検証（k-fold cross-validation）
+# kf = KFold(n_splits=5, shuffle=True)
+# print(train_val_ds)
+# for train_index, test_index in kf.split(train_val_ds):
+#     print(train_index, test_index)
+#
+# # 一つ抜き交差検証（leave-one-out cross-validation）
+# loo = LeaveOneOut()
+# for train, test in loo.split(train_val_ds):
+#     print("train {}, test {}".format(train, test))
+#
+# # 層化k分割交差検証（stratified k-fold cross-validation）
+# skf = StratifiedKFold(n_splits=3)
+# for train, test in skf.split(train_val_ds):
+#     print("train {}, test {}".format(train, test))
 
 
 class BERT(nn.Module):
@@ -135,11 +159,9 @@ def load_metrics(load_path):
 def train(model,
           optimizer,
           criterion=nn.CrossEntropyLoss(),
-          train_loader=train_iter,
-          valid_loader=valid_iter,
-          num_epochs=100,
-          eval_every=len(train_iter) // 2,
           file_path=destination_folder,
+          num_epochs=100,
+          n_folds=5,
           best_valid_loss=float("Inf")):
     # initialize running values
     running_loss = 0.0
@@ -155,72 +177,87 @@ def train(model,
     # training loop
     model.train()
     for epoch in range(num_epochs):
-        for (text, labels), _ in train_loader:
-            labels = labels.type(torch.LongTensor)
-            labels = labels.to(device)
-            text = text.type(torch.LongTensor)
-            text = text.to(device)
-            output = model(text, labels)
-            loss, pred = output
+        train_val_generator, test_dataset = get_dataset(
+            fix_length=100, lower=True, vectors="fasttext.en.300d",
+            n_folds=n_folds, seed=123
+        )
+        for fold, (train_dataset, val_dataset) in enumerate(train_val_generator):
+            # training step
+            for batch in get_iterator(
+                    train_dataset, batch_size=batch_size, train=True,
+                    shuffle=True, repeat=False
+            ):
+                text = torch.transpose(batch.description.data, 0, 1)
+                labels = batch.jobflag
+                labels = labels.type(torch.LongTensor)
+                labels = labels.to(device)
+                text = text.type(torch.LongTensor)
+                text = text.to(device)
+                output = model(text, labels)
+                loss, pred = output
 
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
 
-            # update running values
-            running_loss += loss.item()
-            label_cpu = labels.cpu()
-            pred_cpu = torch.argmax(pred, 1).cpu()
-            running_f1_score += f1_score(label_cpu, pred_cpu, average='macro')
-            global_step += 1
+                # update running values
+                running_loss += loss.item()
+                label_cpu = labels.cpu()
+                pred_cpu = torch.argmax(pred, 1).cpu()
+                running_f1_score += f1_score(label_cpu, pred_cpu, average='macro')
+                global_step += 1
 
             # evaluation step
-            if global_step % eval_every == 0:
-                model.eval()
-                with torch.no_grad():
+            model.eval()
+            with torch.no_grad():
+                # validation loop
+                for batch in get_iterator(
+                        train_dataset, batch_size=batch_size, train=True,
+                        shuffle=True, repeat=False
+                ):
+                    text = torch.transpose(batch.description.data, 0, 1)
+                    labels = batch.jobflag
+                    labels = labels.type(torch.LongTensor)
+                    labels = labels.to(device)
+                    text = text.type(torch.LongTensor)
+                    text = text.to(device)
+                    output = model(text, labels)
+                    loss, preds = output
 
-                    # validation loop
-                    for (text, labels), _ in valid_loader:
-                        labels = labels.type(torch.LongTensor)
-                        labels = labels.to(device)
-                        text = text.type(torch.LongTensor)
-                        text = text.to(device)
-                        output = model(text, labels)
-                        loss, preds = output
+                    valid_running_loss += loss.item()
+                    labels_cpu = labels.cpu()
+                    preds_cpu = torch.argmax(preds, 1).cpu()
+                    valid_running_f1_score += f1_score(labels_cpu, preds_cpu, average='macro')
 
-                        valid_running_loss += loss.item()
-                        labels_cpu = labels.cpu()
-                        preds_cpu = torch.argmax(preds, 1).cpu()
-                        valid_running_f1_score += f1_score(labels_cpu, preds_cpu, average='macro')
+            # evaluation
+            average_train_loss = running_loss / train_dataset
+            average_f1_score = running_f1_score / train_dataset
+            average_valid_loss = valid_running_loss / len(val_dataset)
+            average_valid_f1_score = valid_running_f1_score / len(val_dataset)
+            train_loss_list.append(average_train_loss)
+            valid_loss_list.append(average_valid_loss)
+            valid_f1_score_list.append(average_f1_score)
+            global_steps_list.append(global_step)
 
-                # evaluation
-                average_train_loss = running_loss / eval_every
-                average_f1_score = running_f1_score / eval_every
-                average_valid_loss = valid_running_loss / len(valid_loader)
-                average_valid_f1_score = valid_running_f1_score / len(valid_loader)
-                train_loss_list.append(average_train_loss)
-                valid_loss_list.append(average_valid_loss)
-                valid_f1_score_list.append(average_f1_score)
-                global_steps_list.append(global_step)
+            # resetting running values
+            running_loss = 0.0
+            running_f1_score = 0.0
+            valid_running_loss = 0.0
+            valid_running_f1_score = 0.0
+            model.train()
 
-                # resetting running values
-                running_loss = 0.0
-                running_f1_score = 0.0
-                valid_running_loss = 0.0
-                valid_running_f1_score = 0.0
-                model.train()
+            # print progress
+            print(
+                'Epoch [{}/{}], fold {}/{}, Train Loss: {:.4f}, Valid Loss: {:.4f}, Train F1 Score: {:.4f}, Valid F1 Score: {:.4f}'
+                    .format(epoch + 1, num_epochs, fold, n_folds,
+                            average_train_loss, average_valid_loss, average_f1_score, average_valid_f1_score))
 
-                # print progress
-                print(
-                    'Epoch [{}/{}], Step [{}/{}], Train Loss: {:.4f}, Valid Loss: {:.4f}, Train F1 Score: {:.4f}, Valid F1 Score: {:.4f}'
-                        .format(epoch + 1, num_epochs, global_step, num_epochs * len(train_loader),
-                                average_train_loss, average_valid_loss, average_f1_score, average_valid_f1_score))
-
-                # checkpoint
-                if best_valid_loss > average_valid_loss:
-                    best_valid_loss = average_valid_loss
-                    save_checkpoint(file_path + '/' + 'model.pt', model, best_valid_loss)
-                    save_metrics(file_path + '/' + 'metrics.pt', train_loss_list, valid_loss_list, global_steps_list)
+            # checkpoint
+            if best_valid_loss > average_valid_loss:
+                best_valid_loss = average_valid_loss
+                save_checkpoint(file_path + '/' + 'model.pt', model, best_valid_loss)
+                save_metrics(file_path + '/' + 'metrics.pt', train_loss_list, valid_loss_list,
+                             global_steps_list)
 
     save_metrics(file_path + '/' + 'metrics.pt', train_loss_list, valid_loss_list, global_steps_list)
     print('Finished Training!')
