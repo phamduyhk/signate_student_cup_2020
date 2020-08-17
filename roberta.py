c2721246d73263d7bf31f5163c48ad0b6c40063a
phamduyhk <phamduyhk@gmail.com>
2020-08-17 15:46:01 +0900

    up

--- roberta.py
+++ roberta.py
@@ -4,7 +4,7 @@ import collections
 
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import StratifiedKFold,train_test_split
 from sklearn.metrics import classification_report, f1_score, confusion_matrix
 from tqdm.notebook import tqdm
 import matplotlib.pyplot as plt
