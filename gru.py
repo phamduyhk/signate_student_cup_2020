8793664ae84312930908bed89178298db52892af
phamduyhk <phamduyhk@gmail.com>
2020-08-24 18:02:38 +0900

    gru

--- gru.py
+++ gru.py
@@ -6,7 +6,7 @@ from keras.backend import argmax
 from keras.utils import to_categorical
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from sklearn.preprocessing import LabelEncoder
-from keras.callbacks import Callback
+from keras.callbacks import Callback, ModelCheckpoint
 from keras.preprocessing import text, sequence
 from keras.layers import CuDNNLSTM, Bidirectional, GlobalMaxPooling1D, GlobalAveragePooling1D, CuDNNGRU, Conv1D
 from keras.layers import Input, Dense, Embedding, SpatialDropout1D, add, concatenate
@@ -23,6 +23,7 @@ import pandas as pd
 import keras
 from keras import backend as K
 import numpy as np
+from imblearn.under_sampling import RandomUnderSampler
 from keras.wrappers.scikit_learn import KerasClassifier
 
 np.random.seed(42)
@@ -35,12 +36,12 @@ warnings.filterwarnings(
 os.environ['OMP_NUM_THREADS'] = '4'
 
 EMBEDDING_FILE = 'data/crawl-300d-2M.vec'
-BATCH_SIZE = 512
-LSTM_UNITS = 40
+BATCH_SIZE = 128
+LSTM_UNITS = 80
 DENSE_HIDDEN_UNITS = 4 * LSTM_UNITS
-EPOCHS = 15
+EPOCHS = 50
 DROP_PROB = 0.2
-MAX_LEN = 192
+MAX_LEN = 256
 # EMBEDDING_FILE = 'data/wiki-news-300d-1M.vec'
 
 
@@ -50,7 +51,7 @@ def preprocessing_text(df, is_train=True):
 
     for i, line in enumerate(df['description']):
         for r in remove_list:
-            df['description'][i] = df['description'][i].replace(r, "")
+            df['description'][i] = df['description'][i].replace(r, " ")
     # remove duplicated rows
     if is_train:
         df = df.drop_duplicates(subset=['description'])
@@ -58,7 +59,7 @@ def preprocessing_text(df, is_train=True):
     return df
 
 
-train = pd.read_csv('data/pseudo_train.csv')
+train = pd.read_csv('data/data_augmentation_using_language_translation.csv')
 train = preprocessing_text(train)
 test = pd.read_csv('data/test.csv')
 test = preprocessing_text(test, is_train=False)
@@ -68,64 +69,61 @@ X_train = train["description"].fillna("fillna").values
 y_train = train["jobflag"].values
 X_test = test["description"].fillna("fillna").values
 
-X_tra = []
-X_val = []
-y_tra, y_val = [], []
-label_count = {'1': 0, '2': 0, '3': 0, '4': 0}
-# {'1': 624, '2': 348, '3': 1376, '4': 583}
-for i, v in enumerate(y_train):
-    if v == 1:
-        if label_count['1'] <= 340:
-            y_tra.append(v)
-            X_tra.append(X_train[i])
-            label_count['1'] += 1
-        else:
-            y_val.append(v)
-            X_val.append(X_train[i])
-            label_count['1'] += 1
-    if v == 2:
-        if label_count['2'] <= 340:
-            y_tra.append(v)
-            X_tra.append(X_train[i])
-            label_count['2'] += 1
-        else:
-            y_val.append(v)
-            X_val.append(X_train[i])
-            label_count['2'] += 1
-    if v == 3:
-        if label_count['3'] <= 340:
-            y_tra.append(v)
-            X_tra.append(X_train[i])
-            label_count['3'] += 1
-        else:
-            y_val.append(v)
-            X_val.append(X_train[i])
-            label_count['3'] += 1
-    if v == 4:
-        if label_count['4'] <= 340:
-            y_tra.append(v)
-            X_tra.append(X_train[i])
-            label_count['4'] += 1
-        else:
-            y_val.append(v)
-            X_val.append(X_train[i])
-            label_count['4'] += 1
+# X_tra = []
+# X_val = []
+# y_tra, y_val = [], []
+# label_count = {'1': 0, '2': 0, '3': 0, '4': 0}
+# # {'1': 624, '2': 348, '3': 1376, '4': 583}
+# TAKE_NUMBER = 360
+# for i, v in enumerate(y_train):
+#     if v == 1:
+#         if label_count['1'] <= TAKE_NUMBER:
+#             y_tra.append(v)
+#             X_tra.append(X_train[i])
+#             label_count['1'] += 1
+#         else:
+#             y_val.append(v)
+#             X_val.append(X_train[i])
+#             label_count['1'] += 1
+#     if v == 2:
+#         if label_count['2'] <= TAKE_NUMBER:
+#             y_tra.append(v)
+#             X_tra.append(X_train[i])
+#             label_count['2'] += 1
+#         else:
+#             y_val.append(v)
+#             X_val.append(X_train[i])
+#             label_count['2'] += 1
+#     if v == 3:
+#         if label_count['3'] <= TAKE_NUMBER:
+#             y_tra.append(v)
+#             X_tra.append(X_train[i])
+#             label_count['3'] += 1
+#         else:
+#             y_val.append(v)
+#             X_val.append(X_train[i])
+#             label_count['3'] += 1
+#     if v == 4:
+#         if label_count['4'] <= TAKE_NUMBER:
+#             y_tra.append(v)
+#             X_tra.append(X_train[i])
+#             label_count['4'] += 1
+#         else:
+#             y_val.append(v)
+#             X_val.append(X_train[i])
+#             label_count['4'] += 1
+#
+# onehot_encoder = OneHotEncoder(sparse=False)
+# reshaped = y_tra.reshape(len(y_tra), 1)
+# y_tra = onehot_encoder.fit_transform(reshaped)
+#
+# onehot_encoder = OneHotEncoder(sparse=False)
+# y_val_reshaped = y_val.reshape(len(y_val), 1)
+# y_val = onehot_encoder.fit_transform(y_val_reshaped)
 
-y_tra = np.array(y_tra)
-y_val = np.array(y_val)
-onehot_encoder = OneHotEncoder(sparse=False)
-reshaped = y_tra.reshape(len(y_tra), 1)
-y_tra = onehot_encoder.fit_transform(reshaped)
 
-onehot_encoder = OneHotEncoder(sparse=False)
-y_val_reshaped = y_val.reshape(len(y_val), 1)
-y_val = onehot_encoder.fit_transform(y_val_reshaped)
-
-print(y_tra.shape)
-print(y_val.shape)
 # SETTINGS
 max_features = 30000
-maxlen = 192
 embed_size = 300
 
 # Define per-fold score containers <-- these are new
@@ -140,6 +138,11 @@ def z_normalize(data):
     return scaler.transform(data)
 
 
+onehot_encoder = OneHotEncoder(sparse=False)
+
+X_tra, X_val, y_tra, y_val = train_test_split(
+    X_train, y_train, test_size=0.2, random_state=0)
+
 tokenizer = text.Tokenizer(num_words=max_features)
 tokenizer.fit_on_texts(list(X_train) + list(X_test))
 
@@ -147,16 +150,26 @@ X_tra = tokenizer.texts_to_sequences(X_tra)
 X_val = tokenizer.texts_to_sequences(X_val)
 X_test = tokenizer.texts_to_sequences(X_test)
 
-X_tra = sequence.pad_sequences(X_tra, maxlen=maxlen)
-X_val = sequence.pad_sequences(X_val, maxlen=maxlen)
-x_test = sequence.pad_sequences(X_test, maxlen=maxlen)
+X_tra = sequence.pad_sequences(X_tra, maxlen=MAX_LEN)
+X_val = sequence.pad_sequences(X_val, maxlen=MAX_LEN)
+x_test = sequence.pad_sequences(X_test, maxlen=MAX_LEN)
 
 # for previous version
 X_train = tokenizer.texts_to_sequences(X_train)
-x_train = sequence.pad_sequences(X_train, maxlen=maxlen)
+x_train = sequence.pad_sequences(X_train, maxlen=MAX_LEN)
 reshaped = y_train.reshape(len(y_train), 1)
 y_train_onehot = onehot_encoder.fit_transform(reshaped)
 
+
+
+# Under sampling
+rus = RandomUnderSampler(random_state=0)
+X_tra, y_tra = rus.fit_sample(X_tra, y_tra)
+
+reshaped = y_tra.reshape(len(y_tra), 1)
+y_tra = onehot_encoder.fit_transform(reshaped)
+y_val_reshaped = y_val.reshape(len(y_val), 1)
+y_val = onehot_encoder.fit_transform(y_val_reshaped)
 
 # x_train = z_normalize(x_train)
 # x_test = z_normalize(x_test)
@@ -204,6 +217,7 @@ class Evaluation(Callback):
             print("\n f1 score - epoch: %d - score: %.6f \n" %
                   (epoch + 1, score))
 
+
 def f1(y_true, y_pred):
     def recall(y_true, y_pred):
         """Recall metric.
@@ -234,8 +248,10 @@ def f1(y_true, y_pred):
     recall = recall(y_true, y_pred)
     return 2*((precision*recall)/(precision+recall+K.epsilon()))
 
+
+
 def get_gru_model():
-    inp = Input(shape=(maxlen,))
+    inp = Input(shape=(MAX_LEN,))
     x = Embedding(nb_words, embed_size, weights=[embedding_matrix])(inp)
     x = SpatialDropout1D(DROP_PROB)(x)
     x = Bidirectional(GRU(LSTM_UNITS, return_sequences=True))(x)
@@ -261,8 +277,7 @@ def get_gru_lstm_model(embedding_matrix):
                   weights=[embedding_matrix], trainable=False)(words)
     x = SpatialDropout1D(DROP_PROB)(x)
     x = Bidirectional(GRU(LSTM_UNITS, return_sequences=True))(x)
-    x = Bidirectional(LSTM(LSTM_UNITS, return_sequences=True))(x)
-
+    x = Bidirectional(LSTM(LSTM_UNITS, return_sequences=True))(x);
     hidden = concatenate([
         GlobalMaxPooling1D()(x),
         GlobalAveragePooling1D()(x),
@@ -274,7 +289,8 @@ def get_gru_lstm_model(embedding_matrix):
     result = Dense(4, activation='softmax')(hidden)
 
     model = Model(inputs=words, outputs=result)
-    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy', f1])
+    model.compile(loss='categorical_crossentropy',
+                  optimizer='adam', metrics=['accuracy', f1])
 
     return model
 
@@ -306,11 +322,12 @@ def train_with_LGBM():
     print(train_features.shape)
     train_sparse_matrix = sfm.fit_transform(train_features, train_target)
     print(train_sparse_matrix.shape)
-    train_sparse_matrix, valid_sparse_matrix, y_train, y_valid = train_test_split(train_sparse_matrix, train_target, test_size=0.05, random_state=144)
+    train_sparse_matrix, valid_sparse_matrix, y_train, y_valid = train_test_split(
+        train_sparse_matrix, train_target, test_size=0.05, random_state=144)
     test_sparse_matrix = sfm.transform(test_features)
     d_train = lgb.Dataset(train_sparse_matrix, label=y_train)
     d_valid = lgb.Dataset(valid_sparse_matrix, label=y_valid)
-    watchlist = [d_train, d_valid]       
+    watchlist = [d_train, d_valid]
     params = {'learning_rate': 0.2,
               'application': 'multiclass',
               'num_leaves': 31,
@@ -321,10 +338,10 @@ def train_with_LGBM():
               'feature_fraction': 0.6,
               'nthread': 4,
               'lambda_l1': 1,
-              'lambda_l2': 1}             
+              'lambda_l2': 1}
     model = lgb.train(params,
                       train_set=d_train,
-                      num_boost_round= 80,
+                      num_boost_round=80,
                       valid_sets=watchlist,
                       verbose_eval=10)
     # stacker = lgb.LGBMClassifier(max_depth=3, n_estimators=125, num_leaves=10, boosting_type="gbdt",
@@ -368,7 +385,7 @@ def train_with_cv(batch_size=32, epochs=5, num_folds=10):
     y_pred = model.predict(x_test, batch_size=1024)
     pred = np.argmax(y_pred, 1)
     submission.iloc[:, 1] = pred + 1
-    submission.to_csv('./output/grulstm_{}ep_{}cv.csv'.format(epochs,
+    submission.to_csv('submission_{}ep_{}cv.csv'.format(epochs,
                                                               num_folds), index=False, header=None)
 
 
@@ -382,14 +399,20 @@ def train(batch_size=32, epochs=10):
     #     x_train, y_train_onehot, train_size=0.80, random_state=233)
     print(y_tra.shape)
     print(y_val.shape)
-    print(X_tra)
+    print(X_tra.shape)
+    
+    cp = ModelCheckpoint("weights.hdf5", monitor="val_loss", verbose=1,
+                     save_best_only=True, save_weights_only=True)
+
     eval_score = Evaluation(validation_data=(X_val, y_val), interval=1)
     hist = model.fit(X_tra, y_tra, batch_size=batch_size, epochs=epochs, validation_data=(X_val, y_val),
                      callbacks=[eval_score], verbose=2)
 
     y_pred = model.predict(x_test, batch_size=1024)
+    final_prob = y_pred
     pred = np.argmax(y_pred, 1)
     submission.iloc[:, 1] = pred + 1
+    submission["probs"] = final_prob.tolist()
     submission.to_csv('submission_{}_{}ep.csv'.format(
         model_type, EPOCHS), index=False, header=None)
     return hist
