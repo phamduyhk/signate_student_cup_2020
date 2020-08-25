16bcfc40da5d13048d0a23d58907dcb0f0b55fbb
phamduyhk <phamduyhk@gmail.com>
2020-08-25 15:48:55 +0900

    bert2

--- bert2.py
+++ bert2.py
@@ -14,7 +14,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 
-from transformers import AutoTokenizer, AutoModel, AdamW, BertConfig, BertModel
+from transformers import AutoTokenizer, AutoModel, AdamW, BertConfig, BertModel, BertForSequenceClassification
 import nlp
 
 SEED = 42
@@ -136,7 +136,7 @@ class Classifier(nn.Module):
         self.config = BertConfig.from_json_file(model_config)
         self.config.output_hidden_states = True
         self.config.hidden_dropout_prob = 0.1
-        self.bert = BertModel.from_pretrained(model_name, config=self.config)
+        self.bert = BertForSequenceClassification.from_pretrained(model_name, num_labels=4)
         self.dropout = nn.Dropout(drop_prob)
         # for bert only
         self.linear = nn.Linear(MAX_LENGTH, num_classes)
@@ -210,15 +210,17 @@ class Classifier(nn.Module):
             attention_mask=attention_mask,
             token_type_ids=token_type_ids)
 
-        hidden_states = outputs[2]
+        logits =outputs[0]
+        
+        # hidden_states = outputs[2]
 
-        # bs, seq len, hidden size
-        fuse_hidden = self.get_hidden_states(hidden_states)
-        fuse_hidden_context = fuse_hidden
-        hidden_classification = fuse_hidden[:, 0, :]
-        # #################################################################### direct approach
-        logits = self.get_logits_by_random_dropout(fuse_hidden_context, self.qa_start_end).squeeze(-1)
-        outputs = self.linear(logits)
+        # # bs, seq len, hidden size
+        # fuse_hidden = self.get_hidden_states(hidden_states)
+        # fuse_hidden_context = fuse_hidden
+        # hidden_classification = fuse_hidden[:, 0, :]
+        # # #################################################################### direct approach
+        # logits = self.get_logits_by_random_dropout(fuse_hidden_context, self.qa_start_end).squeeze(-1)
+        # outputs = self.linear(logits)
         return logits
 
 
