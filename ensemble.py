b2a1cb5210b8a2b2271fc7d85922b1bc1f24fb68
phamduyhk <phamduyhk@gmail.com>
2020-08-26 11:23:33 +0900

    up

--- ensemble.py
+++ ensemble.py
@@ -6,13 +6,14 @@ import os
 # "output/prob_bert-base-cased_512_10ep.csv", "output/prob_bert-base-cased_128_10ep.csv"]
 
 
-ensemble_files = ["./output/prob_albert-xxlarge-v2_512_10ep.csv","./output/prob_albert-xxlarge-v2_128_10ep.csv"]
+ensemble_files = ["./output/SC_bert-large-cased_0-192_5cv_5ep.csv", "output/pseudo_bert-large-cased_0-100_5cv_8ep.csv", "output/pseudo_bert-large-cased_0-192_5cv_4ep.csv"
+                  ]
 
 submit = pd.read_csv("./data/submit_sample.csv", header=None)
 all_prob = None
 for file in ensemble_files:
-    df = pd.read_csv(file,header=None)
-    array = np.zeros(shape=(len(df),4))
+    df = pd.read_csv(file, header=None)
+    array = np.zeros(shape=(len(df), 4))
     for i, item in enumerate(df.iloc[:, 2]):
         arr = eval(item)
         array[i] = arr
@@ -26,11 +27,11 @@ for file in ensemble_files:
 
 print(all_prob)
 predict_label = np.argmax(all_prob, axis=1)
-predict_label +=1
+predict_label += 1
 print(predict_label)
-submit.iloc[:,1] = predict_label
+submit.iloc[:, 1] = predict_label
 if not os.path.exists("./output"):
     os.mkdir("./output")
-    
-submit.to_csv("./output/submit_ensemble_{}_{}.csv".format("roberta", "albert"), index=False, header=False)
+
+submit.to_csv("./output/submit_ensemble.csv", index=False, header=False)
 submit.head()
--- make_pseudo_label.py
+++ make_pseudo_label.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
-train = pd.read_csv("./data/train.csv")
+train = pd.read_csv("./data/data_augmentation_using_language_translation.csv")
 test = pd.read_csv("./data/test.csv")
 
 
-trained_output_file = "./output/prob_albert-xxlarge-v2_256_10cv_5ep.csv"
+trained_output_file = "./output/pseudo_bert-large-cased_0-256_5cv_4ep.csv"
 trained_output = pd.read_csv(trained_output_file, header=None)
 pseudo_label = trained_output.iloc[:,1]
 test["jobflag"] = pseudo_label
@@ -15,7 +15,7 @@ all_prob = None
 for i, item in enumerate(trained_output.iloc[:,2]):
     arr = eval(item)
     for v in arr:
-        if v>0.5:
+        if v>0.8:
             row = test.loc[i]
             row["jobflag"] = trained_output.iloc[i,1]
             # print(row)
@@ -23,3 +23,13 @@ for i, item in enumerate(trained_output.iloc[:,2]):
 
 print(train)
 train.to_csv("data/pseudo_train.csv", index=False)
+
+
+
+
+# trained_output_file ="submission_gru_50ep.csv"
+# trained_output = pd.read_csv(trained_output_file, header=None)
+# pseudo_label = trained_output.iloc[:,1]
+# test["jobflag"] = pseudo_label
+# pseudo = pd.concat([train,test])
+# pseudo.to_csv("data/pseudo_train_gru.csv", index=False)
